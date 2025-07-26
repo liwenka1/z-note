973af56a17cb759c23a4cb748759554446aee3a1
liwenka1 <2020583117@qq.com>
2025-07-26 20:23:16 +0800

    refactor: 使用 Tailwind CSS + Shadcn-ui 集成更新样式和组件

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 function Versions(): React.JSX.Element {
   const [versions] = useState(window.electron.process.versions);
 
   return (
     <ul className="versions">
-      <li className="electron-version">Electron v{versions.electron}</li>
-      <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+      <Button variant="outline" className="electron-version">
+        Electron v{versions.electron}
+      </Button>
+      <Button variant="outline" className="chrome-version">
+        Chromium v{versions.chrome}
+      </Button>
+      <Button variant="outline" className="node-version">
+        Node v{versions.node}
+      </Button>
     </ul>
   );
 }
