cb490508481b0f76adcb53859a90a1b8f1193568
liwenka1 <2020583117@qq.com>
2025-08-03 18:32:49 +0800

    feat: 更新主窗口尺寸和最小尺寸设置

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,10 @@ import icon from "../../resources/icon.png?asset";
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === "linux" ? { icon } : {}),
