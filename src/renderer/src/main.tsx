ae437e593293bc071fdc44f3b6074af5f960ad48
liwenka1 <2020583117@qq.com>
2025-09-05 02:30:40 +0800

    feat: 在主入口添加ThemeProvider支持

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "next-themes";
 import { enableMapSet } from "immer";
 
 import { Toaster } from "@renderer/components/ui/sonner";
@@ -33,9 +34,11 @@ if (!rootElement.innerHTML) {
   root.render(
     <StrictMode>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Toaster />
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem storageKey="z-note-theme">
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+          <Toaster />
+        </ThemeProvider>
       </QueryClientProvider>
     </StrictMode>
   );
