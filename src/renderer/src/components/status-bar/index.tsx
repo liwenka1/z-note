6d37e335a7a7ac491de138b1fb1b37be5658d23c
liwenka1 <2020583117@qq.com>
2025-11-02 22:30:03 +0800

    refactor: 修复 StatusBar 组件的边框样式

--- src/renderer/src/components/status-bar/index.tsx
+++ src/renderer/src/components/status-bar/index.tsx
@@ -3,7 +3,7 @@ import { HelpButton } from "./help-button";
 
 export function StatusBar() {
   return (
-    <div className="border-border bg-secondary/30 text-foreground fixed right-0 bottom-0 left-0 z-50 flex h-6 w-full items-center justify-end border-t px-2 text-xs">
+    <div className="border-border/30 bg-secondary/30 text-foreground fixed right-0 bottom-0 left-0 z-50 flex h-6 w-full items-center justify-end border-t px-2 text-xs">
       {/* 笔记信息以及帮助按钮 */}
       <div className="flex items-center gap-2">
         <NoteStatusInfo />
